2e7c183402a6ff0e6ef7f3f98959ec2b60835924
Jan Pavlin <jp8765@student.uni-lj.si>
2018-05-17 09:02:57 +0200

    zgodbi 10 in 11

--- view/VpisaniChoosePredmet.php
+++ view/VpisaniChoosePredmet.php
@@ -2,8 +2,60 @@
 
 <html lang="en">
 <head>
-    <?php include("view/includes/head.php");
-    ?>
+    <?php include("view/includes/head.php");    ?>
+
+    <script>
+        $(document).ready( function () {
+
+            // Override default sorta s custom sortom
+            jQuery.fn.dataTableExt.oSort["slo-desc"] = function (x, y) {
+                return sloCompare(y,x);
+            };
+
+            jQuery.fn.dataTableExt.oSort["slo-asc"] = function (x, y) {
+                return sloCompare(x,y);
+            };
+
+            var oTable = $("#table-subject").DataTable({
+                // Custom definicije za vsak stolpec
+                "aoColumns": [
+                    {
+                        "sClass": "center",
+                        "bSortable": false
+                    },
+                    {
+                        "sClass": "center",
+                        "bSortable": true,
+                    },{
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    }, {
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    },{
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    },{
+                        "sClass": "center",
+                        "bSortable": false,
+                        "sType":"slo"
+                    }
+                ],
+                // Ordering v prvem stolpcu
+                "order": [[ 1, 'asc' ]]
+            });
+
+            // Dinamicni ordering, ko se spremeni sort parameter
+            oTable.on( 'order.dt search.dt', function () {
+                oTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                    cell.innerHTML = i+1;
+                } );
+            } ).draw();
+        } );
+    </script>
 </head>
 <body>
 <section id="container">
@@ -13,7 +65,7 @@
             <div class="row">
                 <div  class="col-xs-12 col-md-6">
                     <h3>Izberite predmet </h3>
-                        <table id="table-izpitov" class="table table-striped table-advance table-hover">
+                        <table id="table-subject" class="table table-striped table-advance table-hover">
                             <thead>
                             <tr>
                                 <th>#</th>
--- view/Vpisaniprikaz.php
+++ view/Vpisaniprikaz.php
@@ -3,6 +3,56 @@
 <html lang="en">
 <head>
     <?php include("view/includes/head.php"); ?>
+
+
+    <script>
+        $(document).ready( function () {
+
+            // Override default sorta s custom sortom
+            jQuery.fn.dataTableExt.oSort["slo-desc"] = function (x, y) {
+                return sloCompare(y,x);
+            };
+
+            jQuery.fn.dataTableExt.oSort["slo-asc"] = function (x, y) {
+                return sloCompare(x,y);
+            };
+
+            var oTable = $("#table-subject").DataTable({
+                // Custom definicije za vsak stolpec
+                "aoColumns": [
+                    {
+                        "sClass": "center",
+                        "bSortable": false
+                    },
+                    {
+                        "sClass": "center",
+                        "bSortable": true,
+                    },{
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    }, {
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    },{
+                        "sClass": "center",
+                        "bSortable": true,
+                        "sType":"slo"
+                    }
+                ],
+                // Ordering v prvem stolpcu
+                "order": [[ 1, 'asc' ]]
+            });
+
+            // Dinamicni ordering, ko se spremeni sort parameter
+            oTable.on( 'order.dt search.dt', function () {
+                oTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                    cell.innerHTML = i+1;
+                } );
+            } ).draw();
+        } );
+        </script>
 </head>
 <body>
 <section id="container">
@@ -50,7 +100,7 @@
                     </table>
 
 
-                    <table id="table-izpitov" class="table table-striped table-advance table-hover">
+                    <table id="table-subject" class="table table-striped table-advance table-hover">
                         <thead>
                         <tr>
                             <th>#</th>
