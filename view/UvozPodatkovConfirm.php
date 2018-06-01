0cfc49fdfbfedce3aea608eec818c3828b265efd
janst <jan.starc@gmail.com>
2018-06-02 00:29:24 +0200

    Scenarij 1 done

--- view/UvozPodatkovConfirm.php
+++ view/UvozPodatkovConfirm.php
@@ -2,6 +2,8 @@
     $_SESSION['mainArray'] = $mainArray;
     $duplikati = 0;
     $display = "visible";
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +11,82 @@
 <html lang="en">
 <head>
     <?php include("view/includes/head.php"); ?>
+    <script>
+
+        $(document).ready( function () {
+
+
+            // Override default sorta s custom sortom
+            jQuery.fn.dataTableExt.oSort["slo-desc"] = function (x, y) {
+                return sloCompare(y,x);
+            };
+
+            jQuery.fn.dataTableExt.oSort["slo-asc"] = function (x, y) {
+                return sloCompare(x,y);
+            };
+
+            var oTable = $("#table-kandidati").DataTable({
+                // Custom definicije za vsak stolpec
+                "aoColumns": [{
+                    "sClass": "center",
+                    "bSortable": false
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }, {
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                },{
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                },{
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                },{
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                },{
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                },{
+                    "sClass": "center",
+                    "bSortable": true,
+                    "sType":"slo"
+                }],
+                // Ordering v prvem stolpcu
+                "order": [[ 1, 'asc' ]]
+            });
+
+            // Dinamicni ordering, ko se spremeni sort parameter
+            oTable.on( 'order.dt search.dt', function () {
+                oTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                    cell.innerHTML = i+1;
+                } );
+            } ).draw();
+        } );
+    </script>
 </head>
 <body>
 <section id="container">
@@ -23,10 +101,10 @@
                         <br>
                         <br>
                         <br>
-                        <table id="table-subject" class="table table-striped table-advance table-hover">
+                        <table id="table-kandidati" class="table table-striped table-advance table-hover">
                             <thead>
                             <tr>
-                                <th>#</th>
+                                <th></th>
                                 <th>Ime</th>
                                 <th>Priimek</th>
                                 <th>Program</th>
@@ -46,7 +124,7 @@
 
                                 if(($value['duplikat'] == "NE" || $value['update'] == "DA") && $value['izkoriscen'] == "NE"){
                                     echo "<tr>".
-                                        "<td>".$key."</td>".
+                                        "<td></td>".
                                         "<td>".$value['ime']."</td>".
                                         "<td>".$value['priimek']."</td>".
                                         "<td>".$value['program']."</td>".
